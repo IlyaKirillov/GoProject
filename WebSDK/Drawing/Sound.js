a246bbd041417136e8477a78f0e2fdbe365934a3
IlyaKirillov <kocmohabt.baduk@gmail.com>
2016-09-30 20:01:27 +0300

    Some changes with sound for GoUniverse project.

--- WebSDK/Drawing/Sound.js
+++ WebSDK/Drawing/Sound.js
@@ -19,6 +19,19 @@ function CBoardSound()
 
     this.NewMessage = null;
 
+	this.Feemale10 = null;
+	this.Feemale9  = null;
+	this.Feemale8  = null;
+	this.Feemale7  = null;
+	this.Feemale6  = null;
+	this.Feemale5  = null;
+	this.Feemale4  = null;
+	this.Feemale3  = null;
+	this.Feemale2  = null;
+	this.Feemale1  = null;
+
+	this.PrevCountDownTime = null;
+
     this.m_bIE      = false;
     this.m_bOn      = true;
 }
@@ -41,6 +54,17 @@ CBoardSound.prototype.Init = function(sPath, bIE)
     this.Capture5   = this.private_AddSound(oBody, "GoBoardApiSoundCapture5",   sPath + "/Capture5.mp3");
     this.CaptureN   = this.private_AddSound(oBody, "GoBoardApiSoundCaptureN",   sPath + "/CaptureN.mp3");
     this.NewMessage = this.private_AddSound(oBody, "GoBoardApiSoundNewMessage", sPath + "/NewMessage.mp3");
+
+	this.Feemale10 = this.private_AddSound(oBody, "GoBoardApiSoundFemale10", "");
+	this.Feemale9  = this.private_AddSound(oBody, "GoBoardApiSoundFemale9", "");
+	this.Feemale8  = this.private_AddSound(oBody, "GoBoardApiSoundFemale8", "");
+	this.Feemale7  = this.private_AddSound(oBody, "GoBoardApiSoundFemale7", "");
+	this.Feemale6  = this.private_AddSound(oBody, "GoBoardApiSoundFemale6", "");
+	this.Feemale5  = this.private_AddSound(oBody, "GoBoardApiSoundFemale5", "");
+	this.Feemale4  = this.private_AddSound(oBody, "GoBoardApiSoundFemale4", "");
+	this.Feemale3  = this.private_AddSound(oBody, "GoBoardApiSoundFemale3", "");
+	this.Feemale2  = this.private_AddSound(oBody, "GoBoardApiSoundFemale2", "");
+	this.Feemale1  = this.private_AddSound(oBody, "GoBoardApiSoundFemale1", "");
 };
 CBoardSound.prototype.Play_PlaceStone = function()
 {
@@ -105,4 +129,28 @@ CBoardSound.prototype.private_PlaySound = function(oAudio)
     catch(e)
     {
     }
+};
+CBoardSound.prototype.private_PlayCountDown = function(nValue)
+{
+	switch (nValue)
+	{
+		case 10: this.private_PlaySound(this.Feemale10); break;
+		case 9: this.private_PlaySound(this.Feemale9); break;
+		case 8: this.private_PlaySound(this.Feemale8); break;
+		case 7: this.private_PlaySound(this.Feemale7); break;
+		case 6: this.private_PlaySound(this.Feemale6); break;
+		case 5: this.private_PlaySound(this.Feemale5); break;
+		case 4: this.private_PlaySound(this.Feemale4); break;
+		case 3: this.private_PlaySound(this.Feemale3); break;
+		case 2: this.private_PlaySound(this.Feemale2); break;
+		case 1: this.private_PlaySound(this.Feemale1); break;
+	}
+};
+CBoardSound.prototype.PlayCountDown = function(sTime)
+{
+	if (sTime < 10.5 && (null === this.PrevCountDownTime || sTime > this.PrevCountDownTime || Math.abs(this.PrevCountDownTime - sTime) > 0.99))
+	{
+		this.PrevCountDownTime = sTime;
+		this.private_PlayCountDown(parseInt(sTime + 0.5));
+	}
 };
